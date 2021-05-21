543ea0b32ab3a7a3ab33591b6ec7465c609acd79
Nicolas Nielsen <nnielsen.dev@gmail.com>
2021-05-21 10:30:48 +0200

    feat(app):add pie chart

--- app/src/app/date-picker/date-picker.component.ts
+++ app/src/app/date-picker/date-picker.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import { Component } from '@angular/core';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 /** @title Datepicker with custom date classes */
 @Component({
@@ -9,25 +9,37 @@ import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 })
 export class DatePickerComponent {
   model: NgbDateStruct;
+
   date: {year: number, month: number};
+
   selectedDate: NgbDateStruct;
+
   body: string;
+
   results: Array<any>;
+
   topics: Array<any>=[];
+
   categories: Array<any>=[];
 
   view: any[] = [1000, 700];
+
   gradient: boolean = true;
+
   showLegend: boolean = false;
+
   showLabels: boolean = true;
+
   isDoughnut: boolean = true;
-  legendPosition: string = 'below';
+
+  // legendPosition: string = 'below';
+
   colorScheme = {
-    domain: ['#47575c', '#366a6d', '#257c70', '#318d66', '#579c4f', '#87a631', '#c0aa05', '#ffa600']
+    domain: ['#47575c', '#366a6d', '#257c70', '#318d66', '#579c4f', '#87a631', '#c0aa05', '#ffa600'],
   };
 
   constructor(private calendar: NgbCalendar) {
-    Object.assign(this, { this:this.categories });
+    Object.assign(this, { this: this.categories });
   }
 
   getProducts() {
@@ -36,30 +48,29 @@ export class DatePickerComponent {
     this.selectedDate = this.model;
     this.body = JSON.stringify(this.selectedDate);
     fetch('http://localhost:8000/products', {
-      method:'POST',
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
 
       body: this.body,
     }).then((res) => res.json())
-    .then((data) => {
-      this.results = data.posts.edges
-      for(let result of this.results) {
-        for(let topicResults of result.node.topics.edges) {
-            this.topics.push({name: topicResults.node.name});
+      .then((data) => {
+        this.results = data.posts.edges;
+        for(const result of this.results) {
+          for(const topicResults of result.node.topics.edges) {
+              this.topics.push({name: topicResults.node.name});
+          }
         }
-      }
-      this.getNumberCategories();
-    })
+        this.getNumberCategories();
+      });
   }
 
   getNumberCategories() {
     this.categories = Object.values(this.topics.reduce((obj:Object, { name }) => {
-      if(obj[name] === undefined) obj[name] = { name: name, value: 1};
+      if (obj[name] === undefined) obj[name] = { name, value: 1 };
       else obj[name].value++
       return obj;
-    },{}))
+    }, {}));
   }
-
 }
